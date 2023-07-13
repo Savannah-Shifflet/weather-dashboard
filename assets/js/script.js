2070ed64aee9b5c3bfe551142a632e79de79609b
unknown <savannah.shifflet@gmail.com>
2023-07-13 18:27:54 -0500

    added event listener for search button to set city name and call coord function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,28 @@
+var searchBtn = $('#searchBtn');
+var cityName = '';
+
 var geoURL = 'https://api.openweathermap.org/data/2.5/forecast?q=';
 var apiKey = '&appid=a6249de91a8f3cbb556fdd4308539cf7';
-var cityName = 'Austin'
 
-fetch(geoURL + cityName + apiKey) 
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data)
-        var latitude = data.city.coord.lat;
-        var longitude = data.city.coord.lon;
-        console.log(latitude)
-        console.log(longitude)
-    })
+var latitude = '';
+var longitude = '';
+
+function cityCoord () {
+    fetch(geoURL + cityName + apiKey) 
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            latitude = data.city.coord.lat.toString();
+            longitude = data.city.coord.lon.toString();
+            console.log(latitude)
+            console.log(longitude)
+        })
+}
+
+$(searchBtn).on("click", function () {
+    cityName = $('#cityName').val();
+    console.log(cityName);
+    cityCoord();
+})
