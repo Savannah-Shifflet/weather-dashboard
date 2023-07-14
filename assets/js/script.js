088c02a309cb962ec77e625d05f931ac53064f4f
unknown <savannah.shifflet@gmail.com>
2023-07-13 19:48:43 -0500

    added icon to current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,10 @@ var cityTitle = $('#cityTitle');
 var todayTemp = $('#todayTemp');
 var todayWind = $('#todayWind');
 var todayHumidity = $('#todayHumidity');
+var todayIcon = $('#todayIcon');
 
 var cityName = '';
-var todaysDate = dayjs().format('MMM D, YYYY');
+var todaysDate = dayjs().format('MMMM D, YYYY');
 
 var geoURL = 'https://api.openweathermap.org/data/2.5/forecast?q=';
 var apiKey = '&appid=a6249de91a8f3cbb556fdd4308539cf7';
@@ -26,6 +27,7 @@ function cityCoord () {
                 console.log(data)
                 latitude = data.city.coord.lat.toString();
                 longitude = data.city.coord.lon.toString();
+                cityName = data.city.name;
                 console.log(latitude)
                 console.log(longitude)
                 cityForecast();
@@ -51,6 +53,8 @@ function cityForecast () {
             $(todayTemp).text('Temp: ' + data.current.temp.toString() + ' °F');
             $(todayWind).text('Wind: ' + data.current.wind_speed.toString() + ' MPH');
             $(todayHumidity).text('Humidity: ' + data.current.humidity.toString() + ' %');
+            var iconSrc = ('https://openweathermap.org/img/w/' + data.current.weather[0].icon + '.png');
+            $(todayIcon).attr('src', iconSrc);
         })
 }
 
