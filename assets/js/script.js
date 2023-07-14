ffd01e43a7be78b745406ae8d932dfb6c09f1fda
unknown <savannah.shifflet@gmail.com>
2023-07-13 19:18:02 -0500

    created function cityForecast to fill in current weather data for city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,17 @@
 var searchBtn = $('#searchBtn');
 var searchCard = $('#searchCard');
+var cityTitle = $('#cityTitle');
+var todayTemp = $('#todayTemp');
+var todayWind = $('#todayWind');
+var todayHumidity = $('#todayHumidity');
+
 var cityName = '';
+var todaysDate = dayjs().format('MMM D, YYYY');
 
 var geoURL = 'https://api.openweathermap.org/data/2.5/forecast?q=';
 var apiKey = '&appid=a6249de91a8f3cbb556fdd4308539cf7';
+var forecastURL = 'https://api.openweathermap.org/data/3.0/onecall?units=imperial&lat='
+var forecastURLLonParameter = '&lon='
 
 var latitude = '';
 var longitude = '';
@@ -20,12 +28,29 @@ function cityCoord () {
                 longitude = data.city.coord.lon.toString();
                 console.log(latitude)
                 console.log(longitude)
+                cityForecast();
             } else {
                 var error = document.createElement('p');
                 $(error).text('Please enter a valid city name');
                 $(error).attr('style', 'color: red; font-style: italic');
                 $(searchCard).append(error);
             }
+            
+        })
+}
+
+function cityForecast () {
+    fetch(forecastURL + latitude + forecastURLLonParameter + longitude + apiKey) 
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            // TODO add weather symbol to title
+            $(cityTitle).text(cityName + ' - ' + todaysDate);
+            $(todayTemp).text('Temp: ' + data.current.temp.toString() + ' °F');
+            $(todayWind).text('Wind: ' + data.current.wind_speed.toString() + ' MPH');
+            $(todayHumidity).text('Humidity: ' + data.current.humidity.toString() + ' %');
         })
 }
 
